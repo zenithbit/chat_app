1c4518d47f47dfe91e8203bbb71860146b26a722
Do Duc Hong Phuc <phucddh@bravebits.vn>
2023-11-09 01:20:43 +0700

    Fix minor bug

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ success: "Express on Vercel" });
 });
-app.use("/api/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json({ success: "Express on user" });
 });
 app.use("/api/chats", chatRoute);
