6a679f5010454dc1deeac68c69636e7bf427f8e5
Do Duc Hong Phuc <phucddh@bravebits.vn>
2023-11-08 09:26:15 +0700

    Fix minor bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
-import Chat from "./pages/Chat.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
+// import Chat from "./pages/Chat.jsx";
+// import Login from "./pages/Login.jsx";
+// import Register from "./pages/Register.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import NavBar from "./components/navbar";
@@ -16,9 +16,9 @@ function App() {
       <NavBar />
       <Container className="text-secondary">
         <Routes>
-          <Route path="/" element={user ? <Chat /> : <Login />} />
+          {/* <Route path="/" element={user ? <Chat /> : <Login />} />
           <Route path="/register" element={user ? <Chat /> : <Register />} />
-          <Route path="/login" element={user ? <Chat /> : <Login />} />
+          <Route path="/login" element={user ? <Chat /> : <Login />} /> */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Container>
